3510d0839ad1490eeb8bd0abfd020bbaa79a0642
krishiran-md <dessaugustecarline@icloud.com>
2026-02-15 16:57:01 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Krishiran MD',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Krishiran MD",
+  botOwner: 'ASUNAX', // Your name
+  ownerNumber: '8096351015', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
